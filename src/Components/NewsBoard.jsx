d18d41cc1ab42a360fe89f8b9cc73fbcd4e801df
lalwanid998 <lalwanid998@gmail.com>
2024-02-22 21:29:22 +0530

    setup gh-pages

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -7,7 +7,7 @@ const NewsBoard = ({category}) => {
     useEffect(() => {
         let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
         fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
-    },[category])
+    },[category]);
 
     return ( 
         <div>
