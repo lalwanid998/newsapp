9fdb08922c5f88f15463cf0678b3510be8e8ed64
lalwanid998 <lalwanid998@gmail.com>
2024-02-22 21:04:46 +0530

    setup gh-pages

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
+import axios from "axios";
 const NewsBoard = ({category}) => {
 
     const [articles,setArticles] = useState([]);
 
     useEffect(() => {
-        let url = `https://newsapi.org/pricing/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        let url = axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`);
         fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
     },[category])
 
