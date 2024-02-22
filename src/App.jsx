2d39f832ef740e22230614bd71cde9f627f89044
lalwanid998 <lalwanid998@gmail.com>
2024-02-23 00:45:53 +0530

    setup gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar';
 import NewsBoard from './Components/NewsBoard';
 
 const App = () => {
-  const [category,setCategory] = useState();
+  const [category,setCategory] = useState("general");
   return(
     <div>
       <Navbar setCategory={setCategory}/>
