1f208afb80894c8216f081b9517cc9ab7382e517
lalwanid998 <lalwanid998@gmail.com>
2024-02-22 21:31:47 +0530

    setup gh-pages

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -7,12 +7,12 @@ const NewsBoard = ({category}) => {
     useEffect(() => {
         let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
         fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
-    },[category]);
+    },[category])
 
     return ( 
         <div>
             <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-            {articles?.map((news,index)=>{
+            {articles.map((news,index)=>{
                 return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
             })}
         </div>
