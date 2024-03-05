95a86fab81551ce28f4934922d38f57c4b461069
lalwanid998 <lalwanid998@gmail.com>
2024-03-05 19:35:37 +0530

    setup gh-pages

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 const NewsBoard = ({category}) => {
-    let api_key = "cb2e4b941d5d4d6586b0ada2c39a8b2a";
+    
     const [articles,setArticles] = useState([]);
 
     useEffect(() => {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${api_key}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
         fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
     },[category])
 
